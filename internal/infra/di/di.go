848bfb6f6424100764f1bb984551c4c153d90467
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2025-01-03 17:31:06 +0300

    added bad words filter

--- internal/infra/di/di.go
+++ internal/infra/di/di.go
@@ -22,6 +22,7 @@ func GetAppConstructors() []interface{} {
 		msg.NewSchema,
 		msg.NewEmitter,
 		msg.NewReceiver,
+		msg.NewProcessor,
 	}
 }
 
--- internal/infra/msg/processor.go
+++ internal/infra/msg/processor.go
@@ -1,45 +1,93 @@
 package msg
 
-//import (
-//	"log"
-//
-//	"github.com/lovoo/goka"
-//)
-//
-//func process(ctx goka.Context, msg any) {
-//	var userLike *UserLike
-//	var ok bool
-//	var userPost *UserPost
-//
-//	if userLike, ok = msg.(*UserLike); !ok || userLike == nil {
-//		return
-//	}
-//
-//	if val := ctx.Value(); val != nil {
-//		userPost = val.(*UserPost)
-//	} else {
-//		userPost = &UserPost{PostLike: make(map[int]bool)}
-//	}
-//
-//	userPost.PostLike[userLike.PostId] = userLike.Like
-//
-//	ctx.SetValue(userPost)
-//	log.Printf("[proc] key: %s,  msg: %v, data in group_table %v \n", ctx.Key(), userLike, userPost)
-//}
-//
-//func runProcessor() {
-//	g := goka.DefineGroup(group,
-//		goka.Input(topic, new(userLikeCodec), process),
-//		goka.Persist(new(userPostCodec)),
-//	)
-//	p, err := goka.NewProcessor(brokers,
-//		g,
-//	)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	err = p.Run(context.Background())
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//}
+import (
+	"context"
+	"encoding/binary"
+	"kafka-messager/internal/domain"
+	"kafka-messager/internal/infra/config"
+	"kafka-messager/internal/infra/repo"
+	"log"
+	"strings"
+
+	"github.com/lovoo/goka"
+	"go.uber.org/zap"
+)
+
+type Processor struct {
+	gp       *goka.Processor
+	conf     config.Config
+	log      *zap.SugaredLogger
+	banWords []string
+	bwr      repo.BanWord
+}
+
+func maskBanWords(msg string, banWords []string) string {
+	res := msg
+	for _, word := range banWords {
+		res = strings.Replace(res, word, "***", -1)
+	}
+	return res
+}
+
+func (p *Processor) getProcess() func(ctx goka.Context, msg any) {
+	return func(ctx goka.Context, msg any) {
+		var (
+			m  *domain.Msg
+			ok bool
+		)
+
+		if m, ok = msg.(*domain.Msg); !ok || m == nil {
+			return
+		}
+
+		m.RecipientId = int64(binary.BigEndian.Uint32([]byte(ctx.Key())))
+		m.Message = maskBanWords(m.Message, p.banWords)
+
+		ctx.SetValue(m)
+	}
+}
+
+func (p *Processor) UpdateBanWords(ctx context.Context) {
+	bw, err := p.bwr.GetList(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p.banWords = make([]string, 0)
+	for _, b := range bw {
+		p.banWords = append(p.banWords, b.Word)
+	}
+}
+
+func (p *Processor) Run(ctx context.Context) {
+	g := goka.DefineGroup(goka.Group(p.conf.MsgFiltered),
+		goka.Input(goka.Stream(p.conf.MsgFilteredBlockUsersTopic), new(Codec), p.getProcess()),
+		goka.Persist(new(Codec)),
+	)
+
+	gp, err := goka.NewProcessor(p.conf.Brokers, g)
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.gp = gp
+
+	err = p.gp.Run(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p.gp = gp
+}
+
+func (p *Processor) Stop() {
+	if p.gp != nil {
+		p.gp.Stop()
+	}
+}
+
+func NewProcessor(conf config.Config, log *zap.SugaredLogger, bwr repo.BanWord) *Processor {
+	var gp *goka.Processor
+	banWords := make([]string, 0)
+
+	return &Processor{gp, conf, log, banWords, bwr}
+}
